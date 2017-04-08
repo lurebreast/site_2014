4a8a9a0ddbf3129f933d59e083bceba1f87c41e5
星河-张贵川\Administrator <zhangguichuan@shinezone.com>
2017-04-08 10:31:55 +0800

    no message

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
  * ERROR REPORTING
  *---------------------------------------------------------------
  *
- * Different environments will require different levels of error reporting.
+ * Different environments will    require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
 
